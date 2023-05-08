80880b744ab89403d2ca604b106a373b23051ed1
Tarek Ahmed Abd El Wahab Shafei <115722766+Tarek1482000@users.noreply.github.com>
2023-05-08 23:26:10 +0300

    Delete Calculator directory

--- Calculator/Debug/calc.d
+++
@@ -1,9 +0,0 @@
-calc.o calc.o: ../calc.c ../calc.h ../std_types.h ../lcd.h ../keypad.h
-
-../calc.h:
-
-../std_types.h:
-
-../lcd.h:
-
-../keypad.h:
--- Calculator/Debug/calculator.d
+++
@@ -1,5 +0,0 @@
-calculator.o calculator.o: ../calculator.c ../calc.h ../std_types.h
-
-../calc.h:
-
-../std_types.h:
