bffb2ce4c6245dab082bf37a942e764fce09f2d2
Tarek Ahmed Abd El Wahab Shafei <115722766+Tarek1482000@users.noreply.github.com>
2023-11-29 14:17:42 +0200

    Delete Calculator directory

--- Calculator/Debug/APP/calculator.d
+++
@@ -1,6 +0,0 @@
-APP/calculator.o APP/calculator.o: ../APP/calculator.c \
-  ../APP/../HAL/calc.h ../APP/../HAL/../mcu_config/std_types.h
-
-../APP/../HAL/calc.h:
-
-../APP/../HAL/../mcu_config/std_types.h:
